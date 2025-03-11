0e4edd1a1523fd1f154b9479110b1df8d164a737
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-03-11 09:33:32 -0300

    feat: add Footer component and integrate it into the main page layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,18 @@
 import Header from "@/components/header"
 import styles from "./page.module.css"
 import Sidebar from "@/components/sidebar"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
     <div>
       <Header />
 
+      <main className={styles.dashboardContainer}>
       <Sidebar />
+      </main>
+
+      <Footer />
     </div>
   )
 }
